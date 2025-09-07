de28b7bfcdcb24a2abc15d0b40280ba108889c0b
AdiistheGoat <145622622+AdiistheGoat@users.noreply.github.com>
2025-09-07 18:50:40 -0500

    added gitignore and changed projects

--- src/projects.js
+++ src/projects.js
@@ -11,38 +11,45 @@ const Projects = () => {
             {[
               {
                 title: "ShortNExact",
-                description: "Designed and built AI-powered pipeline to generate structured questionnaires from YouTube videos using \
-                OpenAI-Whisper and Qwen 2.5.",
+                description: "Designed and built a production-grade agentic AI web application that trims or expands your text to hit the exact word count while preserving meaning, tone, and grammar.\
+                The application is deployed on GCP and is complete with load balancing, api rate limiting, and api key authentication.",
                 image: "",
-                tech: ["Python", "OpenAI-Whisper", "Qwen 2.5", "yt-dlp"],
+                tech: ["Python", "FastAPI", "Gradio","Docker", "Postman","Redis","HAProxy","PostgreSQL","GCP"],
                 github: "https://github.com/AdiistheGoat/ShortNExact"
               },
 
               {
                 title: "Myshrooms",
-                description: "Designed and built AI-powered pipeline to generate structured questionnaires from YouTube videos using \
-                OpenAI-Whisper and Qwen 2.5.",
+                description: "Developed a poisonous mushroom classification app using React Native and Flask, hosted on Expo.",
                 image: "",
-                tech: ["Python", "OpenAI-Whisper", "Qwen 2.5", "yt-dlp"],
-                github: "https://github.com/AdiistheGoat/ShortNExact"
+                tech: ["Python","React-Native", "scikit-learn", "OpenCV", "Pandas"],
+                github: "https://github.com/AdiistheGoat/Myshrooms"
               },
-
 
               {
                 title: "LLM finetuning using LORA",
-                description: "Designed and built AI-powered pipeline to generate structured questionnaires from YouTube videos using \
-                OpenAI-Whisper and Qwen 2.5.",
+                description: "Fine-tuned FLAN-T5 Large using LoRA—adding trainable low-rank adapters to replace linear layers while freezing the base model and training only the LoRA parameters.\
+                The workflow demonstrates efficient fine-tuning with attention masks and padding to handle variable-length text data.",
                 image: "",
-                tech: ["Python", "OpenAI-Whisper", "Qwen 2.5", "yt-dlp"],
-                github: "https://github.com/AdiistheGoat/ShortNExact"
+                tech: ["Python", "Pytorch", "Pandas", "Jupyter"],
+                github: "https://github.com/AdiistheGoat/fine_tuning_using_LORA"
               },
 
               {
                 title: "Spark-based Loans Application Analysis",
-                description: "Built and deployed a web app on GCP that computes shortest paths and \
-                nearest points (campus buildings) using Dijkstra's algorithm, with a responsive UI and robust JUnit5 testing",
+                description: "Built a scalable Apache Spark + Hive pipeline for 100K+ financial records, optimized with bucketing, partitioning, and in-memory caching, \
+                and deployed on a Dockerized distributed cluster. ",
                 image: "",
-                tech: ["Java", "GCP", "JUnit", "HTML/CSS"],
+                tech: ["Python", "Docker", "ApacheSpark", "HTML/CSS"],
+                github: ""
+              },
+
+              {
+                title: "Network Path Finder",
+                description: "Designed and deployed a full-stack web application on a GCP virtual machine, \
+                allowing users to find the shortest path between two points and determine the closest points from a specified location using Dijkstra’s algorithm.",
+                image: "",
+                tech: ["Java", "HTML", "CSS", "Junit5", "GCP"],
                 github: ""
               }
 
